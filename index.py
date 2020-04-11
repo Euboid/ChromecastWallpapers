9ac78e1667001bc6863a8f23269f13621ec7957b
Wilson Wilson <wilson@Wilsons-MacBook-Air.local>
2020-04-11 18:24:47 +0100

    Added image saving. Allows user to pick number of images to download. Allows user to pick chromecast server. Displays progress.

--- index.py
+++ index.py
@@ -1,11 +1,42 @@
 import requests
 import re
 import time
+from PIL import Image
+from io import BytesIO
+
+links = list()
+
+# Get Number of Chromecast Images User Would Like
+nos = int(input("How many chromecast images would you like to download? "))
+server = int(input("Pick Server (3 - 6): "))
+cur = 0
 
 while True:
-    r = requests.get("https://clients3.google.com/cast/chromecast/home")
-    match = re.search(r"(lh4\.googl.+?mv)", r.text).group(1)
-    match = match.replace("\\", "")
-    match = match.replace("u003d", "=")
-    print(match)
-    time.sleep(2)
+    if cur == nos:
+        break
+    else:
+        r = requests.get("https://clients3.google.com/cast/chromecast/home")
+        match = re.search(r"(lh{}\.googl.+?mv)".format(str(server)), r.text).group(1)
+        match = match.replace("\\", "")
+        match = match.replace("u003d", "=")
+        match = 'https://' + match
+        links.append(match)
+        print("Extracting link: " + str(cur + 1))
+        time.sleep(1)
+        cur += 1
+
+
+ident = 0
+unique_list = list()
+for link in links:
+    ident += 1
+    # Check if image is unique
+    if link not in unique_list:
+        print("Saving Image", str(ident))
+        # Save file
+        try:
+            response = requests.get(link)
+            img = Image.open(BytesIO(response.content))
+            img.save('chromecast_i' + str(ident) + '.png')
+        except Exception:
+            print("Bad IMAGE!")
